32b22a7604d913a9a16e98da653241b0c20da959
Jeffrey Way <jeffrey@jeffrey-way.com>
2014-04-21 09:02:35 -0400

    Add tests for dynamic attribute replacer

---
+++ spec/Laracasts/TestDummy/DynamicAttributeReplacerSpec.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace spec\Laracasts\TestDummy;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class DynamicAttributeReplacerSpec extends ObjectBehavior
+{
+	function it_is_initializable()
+	{
+		$this->shouldHaveType('Laracasts\TestDummy\DynamicAttributeReplacer');
+	}
+
+	function it_updates_an_array_that_contains_placeholders()
+	{
+		$replaced = $this->replace([
+			'title' => 'Some Post Number $integer',
+			'body' => '$text',
+			'excerpt' => 'Some excerpt with $string',
+			'author' => '$invalid placeholder'
+		]);
+
+		$replaced->shouldBeArray();
+
+		$replaced['title']->shouldEqual('Some Post Number 1');
+		$replaced['body']->shouldNotEqual('$text');
+		$replaced['excerpt']->shouldMatch('Some excerpt with [a-z]+$');
+		$replaced['author']->shouldEqual('$invalid placeholder');
+	}
+
+	public function getMatchers()
+	{
+		return [
+			'match' => function($output, $input)
+			{
+				return preg_match('/' . $input . '/', $output);
+			}
+		];
+	}
+}
