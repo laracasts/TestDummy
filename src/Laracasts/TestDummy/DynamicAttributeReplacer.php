e970d9243f237e1a88fad674c552d8345a75ac48
Jeroen Noten <jeroennoten@me.com>
2014-08-28 17:15:58 +0200

    Fixed bug regarding NULL values overrides

--- src/Laracasts/TestDummy/DynamicAttributeReplacer.php
+++ src/Laracasts/TestDummy/DynamicAttributeReplacer.php
@@ -47,7 +47,9 @@ class DynamicAttributeReplacer {
 	{
 		foreach ($data as $column => $value)
 		{
-			$data[$column] = $this->updateColumnValue($value);
+			if (is_string($value)) {
+				$data[$column] = $this->updateColumnValue($value);
+			}
 		}
 
 		return $data;
