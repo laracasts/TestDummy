3f5beb316ca8d0c5598527bdc8057df5df9dc7ef
Jeffrey Way <jeffrey@jeffrey-way.com>
2014-09-27 16:58:10 -0400

    Spaces

--- src/Laracasts/TestDummy/Factory.php
+++ src/Laracasts/TestDummy/Factory.php
@@ -8,96 +8,96 @@ use Symfony\Component\Yaml\Yaml;
  */
 class Factory {
 
-	/**
-	 * The path to the fixtures file.
-	 *
-	 * @var array
-	 */
-	protected static $fixtures;
+    /**
+     * The path to the fixtures file.
+     *
+     * @var array
+     */
+    protected static $fixtures;
 
-	/**
-	 * The persistence layer.
+    /**
+     * The persistence layer.
      *
      * @var BuildableRepositoryInterface
-	 */
-	protected static $databaseProvider;
+     */
+    protected static $databaseProvider;
 
-	/**
-	 * Create a new Builder instance.
-	 *
-	 * @return Builder
-	 */
-	protected static function getInstance()
-	{
-		if ( ! static::$fixtures) static::setFixtures();
-		if ( ! static::$databaseProvider) static::setDatabaseProvider();
+    /**
+     * Create a new Builder instance.
+     *
+     * @return Builder
+     */
+    protected static function getInstance()
+    {
+        if ( ! static::$fixtures) static::setFixtures();
+        if ( ! static::$databaseProvider) static::setDatabaseProvider();
 
-		return new Builder(static::$databaseProvider, static::$fixtures);
-	}
+        return new Builder(static::$databaseProvider, static::$fixtures);
+    }
 
-	/**
-	 * Fill an entity with test data, without saving it.
-	 *
-	 * @param string $type
-	 * @param array  $attributes
-	 * @return array
-	 */
-	public static function build($type, array $attributes = [])
-	{
-		return static::getInstance()->build($type, $attributes);
-	}
+    /**
+     * Fill an entity with test data, without saving it.
+     *
+     * @param string $type
+     * @param array  $attributes
+     * @return array
+     */
+    public static function build($type, array $attributes = [])
+    {
+        return static::getInstance()->build($type, $attributes);
+    }
 
-	/**
-	 * Fill and save an entity.
-	 *
-	 * @param string $type
-	 * @param array  $attributes
-	 * @return mixed
-	 */
-	public static function create($type, array $attributes = [])
-	{
-		return static::getInstance()->create($type, $attributes);
-	}
+    /**
+     * Fill and save an entity.
+     *
+     * @param string $type
+     * @param array  $attributes
+     * @return mixed
+     */
+    public static function create($type, array $attributes = [])
+    {
+        return static::getInstance()->create($type, $attributes);
+    }
 
-	/**
-	 * Set the number of times to create a record.
-	 *
-	 * @param $times
-	 * @return $this
-	 */
-	public static function times($times)
-	{
-		return static::getInstance()->setTimes($times);
-	}
+    /**
+     * Set the number of times to create a record.
+     *
+     * @param $times
+     * @return $this
+     */
+    public static function times($times)
+    {
+        return static::getInstance()->setTimes($times);
+    }
 
-	/**
-	 * Set the fixtures path.
-	 *
-	 * @param $basePath
-	 */
-	public static function setFixtures($basePath = null)
-	{
-		if ( ! $basePath)
-		{
-			$basePath = file_exists(base_path('tests')) ? base_path('tests') : app_path('tests');
-		}
+    /**
+     * Set the fixtures path.
+     *
+     * @param $basePath
+     */
+    public static function setFixtures($basePath = null)
+    {
+        if ( ! $basePath)
+        {
+            $basePath = file_exists(base_path('tests')) ? base_path('tests') : app_path('tests');
+        }
 
-		$finder = new FixturesFinder($basePath);
+        $finder = new FixturesFinder($basePath);
 
-		static::$fixtures = Yaml::parse($finder->find());
-	}
+        static::$fixtures = Yaml::parse($finder->find());
+    }
 
-	/**
-	 * Set the database provider.
-	 *
-	 * @param null $provider
-	 * @return EloquentDatabaseProvider
-	 */
-	public static function setDatabaseProvider($provider = null)
-	{
-		$provider = $provider ?: new EloquentDatabaseProvider;
+    /**
+     * Set the database provider.
+     *
+     * @param null $provider
+     * @return EloquentDatabaseProvider
+     */
+    public static function setDatabaseProvider($provider = null)
+    {
+        $provider = $provider ?: new EloquentDatabaseProvider;
 
-		return static::$databaseProvider = $provider;
-	}
+        return static::$databaseProvider = $provider;
+    }
 
 }
--- src/Laracasts/TestDummy/FixturesFinder.php
+++ src/Laracasts/TestDummy/FixturesFinder.php
@@ -5,19 +5,19 @@ use RecursiveIteratorIterator;
 
 class FixturesFinder {
 
-	/**
-	 * The base directory to conduct the search.
-	 *
-	 * @var string
-	 */
-	protected $basePath;
+    /**
+     * The base directory to conduct the search.
+     *
+     * @var string
+     */
+    protected $basePath;
 
-	/**
+    /**
      * The name of the fixtures file.
      *
-	 * @var string
-	 */
-	private $fixturesFileName;
+     * @var string
+     */
+    private $fixturesFileName;
 
     /**
      * Create a new FixturesFinder instance.
@@ -25,51 +25,51 @@ class FixturesFinder {
      * @param $basePath
      * @param string $fixturesFileName
      */
-	function __construct($basePath, $fixturesFileName = 'fixtures')
-	{
-		$this->basePath = $basePath;
-		$this->fixturesFileName = $fixturesFileName;
-	}
+    function __construct($basePath, $fixturesFileName = 'fixtures')
+    {
+        $this->basePath = $basePath;
+        $this->fixturesFileName = $fixturesFileName;
+    }
 
-	/**
-	 * Track down the fixtures file for TestDummy.
-	 * This way, the file can be placed just about anywhere.
-	 *
-	 * @throws TestDummyException
-	 * @return mixed
-	 */
-	public function find()
-	{
-		foreach($this->getDirectoryIterator() as $file)
-		{
-			$name = $file->getFilename();
+    /**
+     * Track down the fixtures file for TestDummy.
+     * This way, the file can be placed just about anywhere.
+     *
+     * @throws TestDummyException
+     * @return mixed
+     */
+    public function find()
+    {
+        foreach($this->getDirectoryIterator() as $file)
+        {
+            $name = $file->getFilename();
 
-			if ($this->isTheFixturesFile($name)) return $file->getPathname();
-		}
+            if ($this->isTheFixturesFile($name)) return $file->getPathname();
+        }
 
-		throw new TestDummyException('Could not locate the fixtures.yml file.');
-	}
+        throw new TestDummyException('Could not locate the fixtures.yml file.');
+    }
 
-	/**
-	 * Get the directory iterator.
-	 *
-	 * @return RecursiveIteratorIterator
-	 */
-	protected function getDirectoryIterator()
-	{
-		return new RecursiveIteratorIterator(
-			new RecursiveDirectoryIterator($this->basePath)
-		);
-	}
+    /**
+     * Get the directory iterator.
+     *
+     * @return RecursiveIteratorIterator
+     */
+    protected function getDirectoryIterator()
+    {
+        return new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($this->basePath)
+        );
+    }
 
-	/**
-	 * Is the given file the one we want?
-	 *
-	 * @param $name
-	 * @return bool
-	 */
-	protected function isTheFixturesFile($name)
-	{
+    /**
+     * Is the given file the one we want?
+     *
+     * @param $name
+     * @return bool
+     */
+    protected function isTheFixturesFile($name)
+    {
         return preg_match('/' . $this->fixturesFileName . '.ya?ml$/i', $name);
-	}
+    }
 }
