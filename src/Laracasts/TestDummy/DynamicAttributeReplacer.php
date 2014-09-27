8a30db88bb951c685efc21257494f2606d375ae0
Jeffrey Way <jeffrey@jeffrey-way.com>
2014-09-27 13:29:41 -0400

    Formatting and indentation

--- src/Laracasts/TestDummy/DynamicAttributeReplacer.php
+++ src/Laracasts/TestDummy/DynamicAttributeReplacer.php
@@ -4,156 +4,157 @@ use Faker\Factory as Faker;
 
 class DynamicAttributeReplacer {
 
-	/**
-	 * We'll increment all numbers references
-	 * to ensure that the same one isn't used.
-	 *
-	 * @var integer
-	 */
-	protected static $number;
-
-	/**
-     * The Faker instance
+    /**
+     * We'll increment all numbers references
+     * to ensure that the same one isn't used.
      *
-	 * @var Faker
-	 */
-	protected $fake;
+     * @var integer
+     */
+    protected static $number;
 
-	/**
-	 * Supported fake types
-	 *
-	 * @var array
-	 */
-	protected $supportedFakes = [
-		'string', 'integer', 'digit', 'date', 'text', 'boolean', 'uuid'
-	];
+    /**
+     * The Faker instance.
+     *
+     * @var Faker
+     */
+    protected $fake;
 
-	/**
-	 * Perform dynamic replacements of text
-	 */
-	function __construct()
-	{
-		$this->fake = Faker::create();
-	}
+    /**
+     * All supported fake types.
+     *
+     * @var array
+     */
+    protected $supportedFakes = [
+        'string', 'integer', 'digit', 'date', 'text', 'boolean', 'uuid'
+    ];
 
-	/**
-	 * Search through an array of attributes,
-	 * and perform any applicable replacements.
-	 *
-	 * @param array $data
-	 * @return array
-	 */
-	public function replace(array $data)
-	{
-		foreach ($data as $column => $value)
-		{
-			if (is_string($value)) {
-				$data[$column] = $this->updateColumnValue($value);
-			}
-		}
+    /**
+     * Create a new DynamicAttributeReplacer instance.
+     */
+    function __construct()
+    {
+        $this->fake = Faker::create();
+    }
 
-		return $data;
-	}
-
-	/**
-	 * Update any placeholders with dynamic fake substitutes.
-	 *
-	 * @param $value
-	 * @return mixed
-	 */
-	protected function updateColumnValue($value)
-	{
-		return preg_replace_callback('/\$([a-z]+)/', function($matches)
-		{
-			if ($this->isASupportedFakeType($fakeType = $matches[1]))
-			{
-				return call_user_func([$this, 'getFake' . ucwords($fakeType)]);
-			}
-
-			// If we don't recognize it, we'll just keep it as it is.
-			return $matches[0];
-		}, $value);
-	}
-
-	/**
-	 * Determine if the provided type is a supported fake type.
-	 *
-	 * @param $fakeType
-	 * @return bool
-	 */
-	protected function isASupportedFakeType($fakeType)
-	{
-		return in_array($fakeType, $this->supportedFakes);
-	}
-
-	/**
-	 * Get a fake string
-	 *
-	 * @return string
-	 */
-	protected function getFakeString()
-	{
-	   return $this->fake->word;
-	}
-
-	/**
-	 * Get a fake integer
-	 *
-	 * @return mixed
-	 */
-	protected function getFakeInteger()
-	{
-		return static::$number += 1;
-	}
-
-	/**
-	 * Get a fake digit
-	 *
-	 * @return mixed
-	 */
-	protected function getFakeDigit()
-	{
-		return rand(0,9);
-	}
-
-	/**
-	 * Get a fake MySQL timestamp
-	 *
-	 * @return mixed
-	 */
-	protected function getFakeDate()
-	{
-		return $this->fake->dateTimeThisYear->format('Y-m-d H:i:s');
-	}
-
-	/**
-	 * Get a fake paragraph
-	 *
-	 * @return mixed
-	 */
-	protected function getFakeText()
-	{
-		return $this->fake->paragraph(4);
-	}
-	
-	/**
-	 * Get a fake boolean
-	 * 
-	 * @return mixed
-	 */
-	protected function getFakeBoolean()
-	{
-		return (int) $this->fake->boolean();
-	}
-	
-	 /**
-         * Get a fake uuid
-         *
-         * @return string
-         */
-        protected function getFakeUuid()
+    /**
+     * Search an array of attributes, and perform any
+     * applicable replacements.
+     *
+     * @param array $data
+     * @return array
+     */
+    public function replace(array $data)
+    {
+        foreach ($data as $column => $value)
         {
-           	return $this->fake->uuid;
+            if (is_string($value))
+            {
+                $data[ $column ] = $this->updateColumnValue($value);
+            }
         }
+
+        return $data;
+    }
+
+    /**
+     * Update any placeholders with fake substitutes.
+     *
+     * @param $value
+     * @return mixed
+     */
+    protected function updateColumnValue($value)
+    {
+        return preg_replace_callback('/\$([a-z]+)/', function ($matches)
+        {
+            if ($this->isASupportedFakeType($fakeType = $matches[1]))
+            {
+                return call_user_func([$this, 'getFake' . ucwords($fakeType)]);
+            }
+
+            // If we don't recognize it, we'll just keep it as it is.
+            return $matches[0];
+        }, $value);
+    }
+
+    /**
+     * Determine if the provided type is a supported fake type.
+     *
+     * @param $fakeType
+     * @return bool
+     */
+    protected function isASupportedFakeType($fakeType)
+    {
+        return in_array($fakeType, $this->supportedFakes);
+    }
+
+    /**
+     * Get a fake string.
+     *
+     * @return string
+     */
+    protected function getFakeString()
+    {
+        return $this->fake->word;
+    }
+
+    /**
+     * Get a fake integer.
+     *
+     * @return mixed
+     */
+    protected function getFakeInteger()
+    {
+        return static::$number += 1;
+    }
+
+    /**
+     * Get a fake digit.
+     *
+     * @return mixed
+     */
+    protected function getFakeDigit()
+    {
+        return rand(0, 9);
+    }
+
+    /**
+     * Get a fake MySQL timestamp.
+     *
+     * @return mixed
+     */
+    protected function getFakeDate()
+    {
+        return $this->fake->dateTimeThisYear->format('Y-m-d H:i:s');
+    }
+
+    /**
+     * Get a fake paragraph.
+     *
+     * @return mixed
+     */
+    protected function getFakeText()
+    {
+        return $this->fake->paragraph(4);
+    }
+
+    /**
+     * Get a fake boolean.
+     *
+     * @return mixed
+     */
+    protected function getFakeBoolean()
+    {
+        return (int) $this->fake->boolean();
+    }
+
+    /**
+     * Get a fake uuid.
+     *
+     * @return string
+     */
+    protected function getFakeUuid()
+    {
+        return $this->fake->uuid;
+    }
 
 }
