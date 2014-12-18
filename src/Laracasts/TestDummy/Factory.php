6efcee4ee9bd24080f938c897a7da169cfab0ced
JeffreyWay <jeffrey@jeffrey-way.com>
2014-12-18 13:24:13 -0500

    Refactor Factory class

--- src/Laracasts/TestDummy/Factory.php
+++ src/Laracasts/TestDummy/Factory.php
@@ -1,43 +1,90 @@
 <?php namespace Laracasts\TestDummy;
 
+use Laracasts\TestDummy\BuildableRepositoryInterface as BuildableRepository;
+
 class Factory {
 
     /**
-     * The path to the factory files.
+     * The path to the factories directory.
+     *
+     * @var string
+     */
+    public static $factoriesPath = 'tests/factories';
+
+    /**
+     * The user registered fatories.
      *
      * @var array
      */
-    private static $factories;
+    private $factories;
 
     /**
      * The persistence layer.
      *
      * @var BuildableRepositoryInterface
      */
-    private static $databaseProvider;
+    public static $databaseProvider;
+
+    /**
+     * Create a new factory instance.
+     *
+     * @param string $factoriesPath
+     * @param BuildableRepository $databaseProvider
+     */
+    public function __construct($factoriesPath = null, BuildableRepository $databaseProvider = null)
+    {
+        if ($factoriesPath)
+        {
+            static::$factoriesPath = $factoriesPath;
+        }
+
+        static::$databaseProvider = $databaseProvider ?: new EloquentDatabaseProvider;
+
+        $this->factories = $this->loadFactories();
+    }
+
+    /**
+     * Get the user registered factories.
+     *
+     * @return array
+     */
+    public function factories()
+    {
+        return $this->factories;
+    }
+
+    /**
+     * Get the database provider.
+     *
+     * @return BuildableRepositoryInterface
+     */
+    public function databaseProvider()
+    {
+        return static::$databaseProvider;
+    }
 
     /**
      * Fill an entity with test data, without saving it.
      *
-     * @param string $type
+     * @param string $name
      * @param array  $attributes
      * @return array
      */
-    public static function build($type, array $attributes = [])
+    public static function build($name, array $attributes = [])
     {
-        return static::instance()->build($type, $attributes);
+        return (new static)->getBuilder()->build($name, $attributes);
     }
 
     /**
      * Fill and save an entity.
      *
-     * @param string $type
+     * @param string $name
      * @param array  $attributes
      * @return mixed
      */
-    public static function create($type, array $attributes = [])
+    public static function create($name, array $attributes = [])
     {
-        return static::instance()->create($type, $attributes);
+        return (new static)->getBuilder()->create($name, $attributes);
     }
 
     /**
@@ -48,50 +95,17 @@ class Factory {
      */
     public static function times($times)
     {
-        return static::instance()->setTimes($times);
+        return (new static)->getBuilder()->setTimes($times);
     }
 
     /**
-     * Create a new Builder instance.
+     * Create a Builder instance.
      *
      * @return Builder
      */
-    private static function instance()
+    private function getBuilder()
     {
-        if ( ! static::$factories) static::setFactoriesPath();
-        if ( ! static::$databaseProvider) static::setDatabaseProvider(new EloquentDatabaseProvider);
-
-        return new Builder(static::$databaseProvider, static::$factories);
-    }
-
-    /**
-     * Set the factories path.
-     *
-     * @param $basePath
-     */
-    public static function setFactoriesPath($basePath = null)
-    {
-        if ( ! $basePath)
-        {
-            $basePath = base_path('tests/factories');
-        }
-
-        if ( ! is_dir($basePath))
-        {
-            throw new TestDummyException("The path provided for the factories, {$basePath}, directory does not exist.");
-        }
-
-        static::loadFactories($basePath);
-    }
-
-    /**
-     * Set the database provider.
-     *
-     * @param BuildableRepositoryInterface $provider
-     */
-    public static function setDatabaseProvider(BuildableRepositoryInterface $provider)
-    {
-        return static::$databaseProvider = $provider;
+        return new Builder($this->databaseProvider(), $this->factories());
     }
 
     /**
@@ -99,23 +113,40 @@ class Factory {
      *
      * @return void
      */
-    public static function loadFactories($basePath)
+    private function loadFactories()
     {
+        $this->assertThatFactoriesDirectoryExists($basePath = static::$factoriesPath);
+
         $designer = new Designer;
         $faker = new FakerAdapter;
-        $finder = new FactoriesFinder($basePath);
 
         $factory = function($name, $shortName, $attributes = []) use ($designer, $faker)
         {
             return $designer->define($name, $shortName, $attributes);
         };
 
-        foreach ($finder->find() as $fixture)
+        foreach ((new FactoriesFinder($basePath))->find() as $fixture)
         {
             include($fixture);
         }
 
-        return static::$factories = $designer->definitions();
+        return $designer->definitions();
+    }
+
+    /**
+     * Assert that the given factories directory exists.
+     *
+     * @param  string $basePath
+     * @return mixed
+     */
+    private function assertThatFactoriesDirectoryExists($basePath)
+    {
+        if ( ! is_dir($basePath))
+        {
+            throw new TestDummyException(
+                "The path provided for the factories, {$basePath}, directory does not exist."
+            );
+        }
     }
 
 }
