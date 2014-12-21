cd30e92cc203581ff28b249c14633941ddad1aa2
JeffreyWay <jeffrey@jeffrey-way.com>
2014-12-21 00:32:27 -0500

    No php 5.5 thing

--- spec/Laracasts/TestDummy/FactoriesLoaderSpec.php
+++ spec/Laracasts/TestDummy/FactoriesLoaderSpec.php
@@ -4,13 +4,12 @@ namespace spec\Laracasts\TestDummy;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
-use Laracasts\TestDummy\TestDummyException;
 
 class FactoriesLoaderSpec extends ObjectBehavior {
 
     function it_gets_pissed_if_the_given_factories_directory_path_does_not_exist()
     {
-        $this->shouldThrow(TestDummyException::class)->duringLoad(__DIR__.'/nonexistent-folder');
+        $this->shouldThrow('Laracasts\TestDummy\TestDummyException')->duringLoad(__DIR__.'/nonexistent-folder');
     }
 
     function it_loads_a_directory_of_user_provided_factories()
