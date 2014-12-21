c6e1617ff64677fd5d2ca63b45dce30153af253b
JeffreyWay <jeffrey@jeffrey-way.com>
2014-12-21 00:29:24 -0500

    Add couple tests for factories loader

---
+++ spec/Laracasts/TestDummy/FactoriesLoaderSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spec\Laracasts\TestDummy;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Laracasts\TestDummy\TestDummyException;
+
+class FactoriesLoaderSpec extends ObjectBehavior {
+
+    function it_gets_pissed_if_the_given_factories_directory_path_does_not_exist()
+    {
+        $this->shouldThrow(TestDummyException::class)->duringLoad(__DIR__.'/nonexistent-folder');
+    }
+
+    function it_loads_a_directory_of_user_provided_factories()
+    {
+        $factories = $this->load(__DIR__.'/helpers');
+
+        $factories->shouldBeArray();
+        $factories->shouldHaveCount(1);
+        $factories[0]->attributes->shouldHaveKey('name');
+        $factories[0]->attributes->shouldHaveKey('artist');
+    }
+
+    public function getMatchers()
+    {
+        return [
+            'haveKey' => function($subject, $key) {
+                return array_key_exists($key, $subject);
+            }
+        ];
+    }
+}
