35619cf75bcfe118ef4f47ca796189c66a02313d
Michgeek <mmichmich@gmail.com>
2015-03-25 10:42:41 +0100

    Allow short name of an existing function

--- tests/support/factories/factories.php
+++ tests/support/factories/factories.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * This function has the same name as the fixture `scheduled_post` and MUST NOT be called
+ * Fixtures with short name should be allowed to use the name of an existing function
+ *
+ * @throws Exception
+ */
+function scheduled_post()
+{
+    throw new \Exception(
+        'Function `scheduled_post()` MUST NOT be called. ' .
+        'Fixture with a short name of an existing function should be allowed.'
+    );
+}
+
 $factory('Post', 'scheduled_post', [
     'title' => 'Scheduled Post Title'
 ]);
